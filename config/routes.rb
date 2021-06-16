5f998b48386778d371dcd805484ae5ec5f5e876f
Jason McKenzie <jmac.atx@gmail.com>
2021-06-16 13:13:38 -0500

    creates a route to test JSON display of all products

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/display_all_products", controller: "products", action: "display_all"
 end
