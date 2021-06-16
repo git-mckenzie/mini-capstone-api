2747bbe9b7afda6a23a1f69eba2ffa1285d6f79b
Jason McKenzie <jmac.atx@gmail.com>
2021-06-16 13:35:37 -0500

    adds paths to show individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,14 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products.as_json
   end
+
+  def display_product1
+    products = Product.first
+    render json: products.as_json
+  end
+
+  def display_product2
+    products = Product.second
+    render json: products.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/display_all_products", controller: "products", action: "display_all"
+
+  get "/display_product1", controller: "products", action: "display_product1"
+
+  get "/display_product2", controller: "products", action: "display_product2"
 end
