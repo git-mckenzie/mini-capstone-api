420ea48f56d90b629ca48df22477ce1f461ac654
Jason McKenzie <jmac.atx@gmail.com>
2021-06-25 14:11:38 -0500

    adds supplier method to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,21 @@
 class Product < ApplicationRecord
-  def is_discounted?
-  end
-
   validates :name, uniqueness: true
   validates :name, :price, :description, :image_url, presence: true
-  validates :price, numericality: true
+  validates :price, numericality: true {greater_than: 0}
+
+  def is_discounted?
+    price <= 10
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
+
+  def supplier
+    Supplier.find_by(id: supplier_id)
+  end
 end
